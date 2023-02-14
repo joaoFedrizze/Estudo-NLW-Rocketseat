72c2a324ebbb11976e5631bd137d4ec176384219
joão vitor Fedrizze <jh.2220@hotmail.com>
2023-02-14 14:09:49 -0300

    aula 4 - Exporer

--- explorer/src/js/script.js
+++ explorer/src/js/script.js
@@ -1,17 +1,42 @@
 const form = document.getElementById("form-habits");
-
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (!dayExists) {
+    alert("Adicionado com sucesso");
+    nlwSetup.addDay(today);
+  } else {
+    alert("Dia já incluso");
+    return;
+  }
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
+
+/*
+=============
+TEMPLATE DATA
+=============
 const data = {
   pizza: ["01-01", "01-02", "01-06"],
   fries: ["01-05", "01-10"],
   hamburguer: ["01-01", "01-03"],
   procastinate: ["01-01", "01-02", "01-08", "01-09"],
   videogame: ["01-01", "01-03", "01-05", "01-06"],
-  hide: ["01-04", "01-07"],
+  web: ["01-04", "01-07"],
   overSleep: ["01-01", "01-02", "01-03", "01-05", "01-06", "01-10"],
 };
-
-nlwSetup.setData(data);
-
-nlwSetup.load();
+*/
